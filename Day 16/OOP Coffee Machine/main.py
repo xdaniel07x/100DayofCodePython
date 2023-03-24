e676c926f6cfcbf03d2e9022edfed64aab9c693f
xdaniel07x <danielwong615@googlemail.com>
2023-03-24 21:42:20 +0000

    Final

--- Day 16/OOP Coffee Machine/main.py
+++ Day 16/OOP Coffee Machine/main.py
@@ -5,3 +5,21 @@ from money_machine import MoneyMachine
 menu = Menu()
 coffee = CoffeeMaker()
 money = MoneyMachine()
+
+
+is_on = True
+
+while is_on:
+
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee.report()
+        money.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee.is_resource_sufficient(drink):
+            money.make_payment(drink.cost)
+            coffee.make_coffee(drink)
