af78466d4a879aad1e4b493f5e4b14a03450c77e
xdaniel07x <danielwong615@googlemail.com>
2023-03-26 16:52:28 +0100

    DAY 17

---
+++ Day 17/Quiz Project/main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from data import question_data
+
+question_bank = []
+for i in question_data:
+    new_q = Question(i["text"], i["answer"])
+    question_bank.append(new_q)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions() == True:
+    quiz.next_question()
+
+quiz.end_game()
