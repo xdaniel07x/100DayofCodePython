c70d2076418fa9a84d0e4d106b41b12d2244ecbd
xdaniel07x <danielwong615@googlemail.com>
2023-03-27 23:14:18 +0100

    Update main.py

--- Day 18/Hirst Painting Project/main.py
+++ Day 18/Hirst Painting Project/main.py
@@ -1,10 +1,5 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
 import colorgram
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+colors = colorgram.extract('image.jpg', 6)
 
-print(rgb_colors)
+print(colors)
