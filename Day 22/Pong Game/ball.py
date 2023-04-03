479e8f258607bcd93b0d46fe1bd63b40987be891
xdaniel07x <danielwong615@googlemail.com>
2023-04-03 21:09:36 +0100

    commit

--- Day 22/Pong Game/ball.py
+++ Day 22/Pong Game/ball.py
@@ -8,8 +8,16 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.move_xcor = 10
+        self.move_ycor = 10
 
     def move_ball(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.move_xcor
+        new_y = self.ycor() + self.move_ycor
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.move_ycor *= -1
+
+    def bounce2(self):
+        self.move_ycor *= 1
--- Day 22/Pong Game/main.py
+++ Day 22/Pong Game/main.py
@@ -22,11 +22,17 @@ screen.onkeypress(l_paddle.down, "s")
 screen.onkeypress(r_paddle.up, "Up")
 screen.onkeypress(r_paddle.down, "Down")
 
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.05)
     screen.update()
     ball.move_ball()
 
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce()
+
+    if ball.distance(r_paddle) < 10:
+        ball.bounce_2()
 
 screen.exitonclick()
