c3b370a9e930690b59bdfc75d92390427062eb27
xdaniel07x <danielwong615@googlemail.com>
2023-04-04 16:17:08 +0100

    Adding some player behavior and player turtle

--- Day 23/car_manager.py
+++ Day 23/car_manager.py
@@ -1,7 +1,14 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        pass
+
+    def create_car(self):
