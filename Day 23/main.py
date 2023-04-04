dc0ecf927543bf788c6a384cedcf95ef413e3f06
xdaniel07x <danielwong615@googlemail.com>
2023-04-04 16:17:11 +0100

    player behavior and others

--- Day 23/main.py
+++ Day 23/main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.move_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player.ycor() >= 280:
+        player.reset_position()
